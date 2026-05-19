1ec784d0e532cd7076a39239533c6192c6d3b0b9
AiMagic5000 <aimagic5000@gmail.com>
2026-05-19 05:01:38 -0700

    revert: training layout padding (hamburger overlap acceptable)

--- src/app/training/layout.tsx
+++ src/app/training/layout.tsx
@@ -9,7 +9,7 @@ export default function TrainingLayout({
     <div className="flex min-h-screen bg-white">
       <TrainingSidebar />
       <main className="flex-1 lg:ml-0">
-        <div className="max-w-4xl mx-auto pl-16 pr-4 pt-16 pb-8 sm:pl-16 sm:pr-6 sm:pt-14 lg:pl-10 lg:pr-10 lg:pt-12 lg:pb-12">
+        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-10 py-8 lg:py-12">
           {children}
         </div>
       </main>
